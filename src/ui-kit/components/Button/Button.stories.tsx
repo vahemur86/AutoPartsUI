df2749438c8a742cf0230d3e65ebf6e2aaa47574
Mariana <mariana.harutyunyan2001@gmail.com>
2025-11-22 02:27:59 +0400

    Removed unused imports

--- src/ui-kit/components/Button/Button.stories.tsx
+++ src/ui-kit/components/Button/Button.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Button } from "./Button";
-import { Save, X, Plus } from "lucide-react";
+import { Save } from "lucide-react";
 
 const meta = {
   title: "UI-Kit/Button",
